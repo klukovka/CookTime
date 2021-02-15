b8583c44a1b6e6de8ea0088b2ca4d3b45bf0ac7d
klukovka <58118015+klukovka@users.noreply.github.com>
2021-02-15 16:27:59 +0200

    Add files via upload

---
+++ Scripts/preload.js
@@ -0,0 +1,15 @@
+﻿var delay_popup = 2000;
+setTimeout("document.getElementById('overlay').style.display='block'", delay_popup);
+
+function change() {
+    window.location = "index.html?max=" + max.value + "&speed=" + speed.value;
+}
+
+jQuery(document).ready(function ($) {
+    $(window).load(function () {
+        setTimeout(function () {
+            $('.preloader').fadeOut('slow', function () { });
+        }, 1000);
+
+    });
+});
